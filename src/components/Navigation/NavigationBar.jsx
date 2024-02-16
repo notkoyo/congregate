d98d93dcc4e07795088c3911586dc68a611ddf32
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-16 12:07:13 +0000

    add navbar

---
+++ src/components/Navigation/NavigationBar.jsx
@@ -0,0 +1,41 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import { CongregateLogo } from "./CongregateLogo";
+
+const navbarItems = ["Meet", "Events"];
+
+export const NavigationBar = () => {
+  return (
+    <Navbar shouldHideOnScroll>
+      <NavbarBrand>
+        <CongregateLogo />
+        <p className="font-bold text-inherit">Congregate</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        {navbarItems.map((item, i) => (
+          <NavbarItem key={i}>
+            <Link color="foreground" href="#">
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
