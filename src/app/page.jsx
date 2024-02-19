f156d98fafd6b310a373c6b2f9055d4330200053
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-19 16:48:03 +0000

    implement auth functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 "use client"
 
+import AuthForm from "@/components/auth-components/AuthFormFinal";
+
 export default function Home() {
 
   return (
     <>
-    
+      <div className="min-h-screen grid place-items-center">
+        <AuthForm />
+      </div>
     </>
   );
 }
---
+++ src/components/auth-components/AuthFormFinal.jsx
@@ -0,0 +1,221 @@
+"use client";
+
+{/*
+
+  - Possible bug with isSigningIn - needs checking
+  - Styling
+
+*/}
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabaseAuth } from "@/utils/supabaseClient";
+
+import { EyeFilledIcon } from "./_eye-icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "./_eye-icons/EyeSlashFilledIcon";
+import { Button, Progress, Input } from "@nextui-org/react";
+
+export default function AuthForm() {
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [password, setPassword] = useState("");
+  const [passwordStrength, setPasswordStrength] = useState(0);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isProgressVisible, setIsProgressVisible] = useState(false);
+
+  const [isNewUser, setIsNewUser] = useState(false);
+  const [isSigningIn, setIsSigningIn] = useState(false);
+  const [isSigningUp, setIsSigningUp] = useState(false);
+
+  const router = useRouter();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabaseAuth.auth.signUp({
+      email,
+      password,
+    });
+    if (!error) {
+      setIsSigningUp(true);
+      setIsSigningIn(false);
+    }
+    console.log({ data, error });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsSigningIn(true);
+    const { data, error } = await supabaseAuth.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (!error) {
+      router.push("/profile");
+    } else {
+      setIsSigningUp(false);
+    }
+    // console.log({ data, error });
+  };
+
+  const handleGoogle = async () => {
+    // console.log("here");
+    const { user, session, error } = await supabaseAuth.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback`,
+      },
+    });
+    if (!error) {
+      router.push("/profile");
+    }
+    // console.log({ user, session, error });
+  };
+
+  let signInMessage = "Login";
+
+  const signUpMessage = (
+    <p className="text-center text-black">
+      Email sent! Check your email to confirm sign up.
+    </p>
+  );
+
+  isNewUser ? (signInMessage = "Sign Up") : undefined;
+
+  const validateEmail = (email) =>
+    email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+
+  const handleEmailChange = (newEmail) => {
+    setEmail(newEmail);
+    if (newEmail === "") {
+      setIsEmailValid(true);
+    } else {
+      setIsEmailValid(validateEmail(newEmail));
+    }
+  };
+
+  let complexityScore = 0;
+
+  const toggleVisibility = () => setIsProgressVisible(!isProgressVisible);
+
+  const validatePassword = (password) =>
+    password.match(
+      /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[\d].*[\d])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/g,
+    );
+
+  const testComplexity = (password) => {
+    complexityScore += /(?=.*[a-z].*[a-z].*[a-z])/g.test(password) ? 25 : 0;
+    complexityScore += /(?=.*[A-Z].*[A-Z])/g.test(password) ? 25 : 0;
+    complexityScore += /(?=.*[!@#$&*])/g.test(password) ? 25 : 0;
+    complexityScore += /(?=.*[\d].*[\d])/g.test(password) ? 25 : 0;
+  };
+
+  const handlePasswordChange = (newPassword) => {
+    testComplexity(newPassword);
+    newPassword === ""
+      ? setPasswordStrength(0)
+      : setPasswordStrength(complexityScore);
+    setPassword(newPassword);
+    newPassword === ""
+      ? setIsPasswordValid(true)
+      : setIsPasswordValid(validatePassword(newPassword));
+  };
+
+  return (
+    <form
+      onSubmit={isNewUser ? handleSignUp : handleLogin}
+      className="flex flex-col gap-4 font-satoshi"
+    >
+      {isNewUser ? <>Create an account</> : <>Login</>}
+      <Input
+        isClearable
+        value={email}
+        type="email"
+        label="Email"
+        variant="faded"
+        isInvalid={!isEmailValid}
+        color={email !== "" ? (isEmailValid ? "success" : "danger") : "default"}
+        errorMessage={!isEmailValid && "Please enter a valid email"}
+        onValueChange={handleEmailChange}
+        className="max-w-xs font-medium"
+      />
+      <Input
+        value={password}
+        label="Password"
+        variant="faded"
+        endContent={
+          <button
+            className="focus:outline-none"
+            type="button"
+            onClick={toggleVisibility}
+          >
+            {isProgressVisible ? (
+              <EyeSlashFilledIcon className="pointer-events-none text-2xl text-default-400" />
+            ) : (
+              <EyeFilledIcon className="pointer-events-none text-2xl text-default-400" />
+            )}
+          </button>
+        }
+        type={isProgressVisible ? "text" : "password"}
+        isInvalid={!isPasswordValid}
+        color={
+          password !== "" ? (isPasswordValid ? "success" : "danger") : "default"
+        }
+        errorMessage={!isPasswordValid && "Please enter a valid password"}
+        onValueChange={handlePasswordChange}
+        className="max-w-xs font-medium"
+      />
+      <Progress
+        aria-label="password strength bar"
+        className={passwordStrength > 0 ? "" : "hidden"}
+        color={passwordStrength === 100 ? "success" : "danger"}
+        size="sm"
+        value={passwordStrength}
+      />
+      <p>
+        {isNewUser ? (
+          <>
+            Already have an Account?{" "}
+            <button
+              className="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"
+              type="button"
+              onClick={() => setIsNewUser(false)}
+            >
+              Sign In
+            </button>
+          </>
+        ) : (
+          <>
+            Don't have an account?{" "}
+            <button
+              className="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"
+              type="button"
+              onClick={() => setIsNewUser(true)}
+            >
+              Sign Up
+            </button>
+          </>
+        )}
+      </p>
+      {isSigningUp && signUpMessage}
+      <Button
+        isLoading={isSigningIn}
+        type="submit"
+        variant="solid"
+        color="primary"
+        className="font-semibold"
+      >
+        {signInMessage}
+      </Button>
+      <div className="flex justify-center">
+        <p>--------or--------</p>
+      </div>
+      <button
+        type="button"
+        className="border-gray rounded bg-slate-200"
+        onClick={handleGoogle}
+      >
+        Google
+      </button>
+    </form>
+  );
+}
