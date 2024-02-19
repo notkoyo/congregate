85b0e539da1e12751ab2645cdc57c0bbd4c5d11b
JoeA <joealexanderadams@gmail.com>
2024-02-19 15:12:06 +0000

    add map and filler for filter

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,27 @@
-"use client"
+"use client";
 
+import { GoogleMap } from "@/components/Maps/GoogleMap";
+import { GoogleMapAutocomplete } from "@/components/Maps/GoogleMapAutocomplete";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedPos, setSelectedPos] = useState({});
 
   return (
-    <>
-
-
-    </>
+    <div className="m-4 mt-8">
+      <div className="flex flex-col gap-2">
+        <section className="flex flex-col gap-2" >
+          <GoogleMap selectedPos={selectedPos} />
+          <GoogleMapAutocomplete setSelectedPos={setSelectedPos} />
+        </section>
+        <section>
+          <div>fill</div>
+          <div>fill</div>
+          <div>fill</div>
+          <div>fill</div>
+          <div>fill</div>
+        </section>
+      </div>
+    </div>
   );
 }
