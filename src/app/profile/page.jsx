8cb6c6c85705ac51fbacb41a226de23c81bb1951
Nim Man <nimjoeman@live.com>
2024-02-19 12:00:38 +0000

    login and signup with email and password

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,10 @@
+import SignOutButton from "@/components/SignOutButton";
+
+export default function Profile() {
+  return (
+    <div>
+      <div>Profile</div>
+      <SignOutButton />
+    </div>
+  );
+}
