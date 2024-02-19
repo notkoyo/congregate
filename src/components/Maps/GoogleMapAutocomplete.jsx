c6baa4b699cfd071eff9738cec3bff7340b1bf3b
JoeA <joealexanderadams@gmail.com>
2024-02-19 15:12:06 +0000

    add map and filler for filter

--- src/components/Maps/GoogleMapAutocomplete.jsx
+++ src/components/Maps/GoogleMapAutocomplete.jsx
@@ -67,7 +67,8 @@ export const GoogleMapAutocomplete = ({ setSelectedPos }) => {
           value={value}
           onChange={handleInput}
           disabled={!ready}
-          placeholder="Where are you going?"
+          placeholder="Search near..."
+          className="border-solid border-black border-1"
         />
         {/* We can use the "status" to decide whether we should display the dropdown or not */}
         {status === "OK" && <ul>{renderSuggestions()}</ul>}
