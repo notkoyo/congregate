1280ba3174c7610b8232976d9ac5cbac6ea4c983
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-20 10:01:42 +0000

    fix icon class names

--- src/components/auth-components/FacebookIcon.jsx
+++ src/components/auth-components/FacebookIcon.jsx
@@ -2,7 +2,7 @@ export default function FacebookIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-brand-facebook"
+      className="icon icon-tabler icon-tabler-brand-facebook"
       width="24"
       height="24"
       viewBox="0 0 24 24"
