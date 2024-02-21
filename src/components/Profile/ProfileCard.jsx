89b6e876b0924ab3f10db06666d5f5bf45b94c66
Nim Man <nimman@192.168.1.169>
2024-02-21 12:40:35 +0000

    merged profile display with profile page

---
+++ src/components/Profile/ProfileCard.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Card, CardBody } from "@nextui-org/react";
+import ProfileDisplay from "./ProfileDisplay";
+
+export default function ProfileCard() {
+  return (
+    <Card className="px-20 py-10 font-satoshi shadow-2xl">
+      <CardBody>
+        <ProfileDisplay />
+      </CardBody>
+    </Card>
+  );
+}
