f2bdf8ba1fa7774f67cc4a8602569ff182f49be2
unknown <dimapen57@gmail.com>
2024-02-21 13:00:11 +0000

    listVenuesShowingVENUeBelongTOAuthUser

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,24 @@
 "use client";
 import React from "react";
+import { useEffect, useState } from "react";
 import { supabaseAuth } from "../../utils/supabaseClient";
 
 function page() {
-  const test = supabaseAuth.auth.getSession();
-  const res = test.then((res) => console.log(res));
-  console.log(res);
-  return <div>page</div>;
+  const [session, setSession] = useState([]);
+  useEffect(() => {
+    const getSessionFunc = async () => {
+      const res = await supabaseAuth.auth.getSession();
+      setSession(res);
+    };
+    getSessionFunc();
+  }, []);
+  console.log(session);
+  return (
+    <div>
+      <p></p>
+      <p></p>
+    </div>
+  );
 }
 
 export default page;
