c3102a8da87c525655abbaaa2032e1ece5680cc9
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-22 12:49:54 +0000

    add hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,45 +1,33 @@
-import { Card, CardBody, Accordion, AccordionItem } from "@nextui-org/react";
+"use client";
 
-const events = [
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-  {
-    event_title: "1",
-  },
-];
+import { useState, useEffect } from "react";
+
+import {
+  Card,
+  CardBody,
+  Accordion,
+  AccordionItem,
+  Image,
+  CardHeader,
+  CardFooter,
+  Spinner,
+} from "@nextui-org/react";
+import { supabaseAuth } from "@/utils/supabaseClient";
+import GitHubIcon from "../Icons/GitHubIcon";
+import FacebookIcon from "../Icons/FacebookIcon";
+import TwitterIcon from "../Icons/XbrandIcon";
+import DiamondIcon from "../Icons/DiamondIcon";
 
 export default function Hero() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    supabaseAuth
+      .from("events")
+      .select("*, venues(photos, city)")
+      .then(({ data }) => setEvents(data));
+  }, []);
+
   const defaultContent =
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda quidem voluptates molestias vero magnam in velit, adipisci distinctio quod eaque! Eum temporibus quaerat praesentium harum quis odio fuga enim veniam.";
 
@@ -88,14 +76,55 @@ export default function Hero() {
         </Accordion>
       </div>
       <div className="min-h-screen bg-cyan-600">
-        <h2 className="mt-12 pt-16 mb-12 font-satoshi text-5xl font-bold text-center text-white">Current Events</h2>
-        <div className="grid grid-cols-1 gap-4 px-28 py-10 sm:px-48 md:grid-cols-2 md:px-40 lg:grid-cols-3">
+        <h2 className="mb-12 mt-12 pt-16 text-center font-satoshi text-5xl font-bold text-white">
+          Current Events
+        </h2>
+        {events.length > 0 ? (
+          <div className="grid grid-cols-1 gap-4 px-20 py-10 pb-24 sm:px-40 md:grid-cols-2 md:px-40 lg:grid-cols-3">
             {events.map((event) => (
-              <Card className="aspect-square max-w-sm">
-                <CardBody>{event.event_title}</CardBody>
+              <Card key={event.event_id} className="aspect-square max-w-sm">
+                <CardBody>
+                  <Image
+                    className="aspect-video max-h-64 w-full"
+                    src={event.venues.photos}
+                    alt="venue image"
+                  />
+                  <div className="mx-auto my-auto text-center font-satoshi text-lg font-bold lg:text-xl xl:text-2xl">
+                    <h3>{event.name}</h3>
+                    <h3>{event.venues.city}</h3>
+                    <h4>{`€ ${event.event_price}`}</h4>
+                  </div>
+                </CardBody>
               </Card>
             ))}
           </div>
+        ) : (
+          <div className="grid place-items-center min-h-96">
+            <div className="text-center">
+              <Spinner size="lg" color="default" className="text-white" />
+              <h2 className="text-3xl font-bold font-satoshi text-white">Loading Current Events...</h2>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="min-h-36 flex justify-between items-center px-20">
+        <span className="font-satoshi font-semibold">© 2024 Congregate Ltd. All rights reserved.</span>
+        <div>
+          <ul className="flex gap-10">
+            <li>
+              <GitHubIcon />
+            </li>
+            <li>
+              <FacebookIcon />
+            </li>
+            <li>
+              <TwitterIcon />
+            </li>
+            <li>
+              <DiamondIcon />
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
