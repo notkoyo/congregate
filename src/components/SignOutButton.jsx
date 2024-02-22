2c2aceb481b2415e61d4f405967761f970120aa1
Nim Man <nimman@192.168.1.169>
2024-02-22 15:01:09 +0000

    created interests component

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,11 +1,15 @@
-export default function SignOutButton() {
+export default function SignOutButton({ text }) {
+  if (!text) {
+    text = "Logout";
+  }
+
   return (
     <form action="/auth/signout" method="post">
       <button
         type="submit"
-        className="inline-block rounded bg-cyan-600 px-5 py-3 font-satoshi text-xl text-white hover:bg-cyan-600"
+        className="inline-block rounded bg-cyan-600 px-5 py-3 font-satoshi text-sm text-white hover:bg-cyan-600"
       >
-        Sign Out
+        {text}
       </button>
     </form>
   );
