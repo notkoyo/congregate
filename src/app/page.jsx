e1e7a223e0ae5ef97caabc938f3879590fc4d3f5
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-22 11:02:51 +0000

    start hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 
+import Hero from "../components/Hero/Hero";
 import { Events } from "../components/Events";
 export default function Home() {
   return (
     <>
-      <Events />
+      <Hero />
     </>
   );
 }
