16d39e167a90e2ee53de7b8fdd4e7aab81c92aa7
JoeA <joealexanderadams@gmail.com>
2024-02-26 11:44:43 +0000

    unify venues list with rest of site, made form data clear when not on form

--- src/components/Venue/ListVenue.jsx
+++ src/components/Venue/ListVenue.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  CardHeader,
+  CardFooter,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+  useDisclosure,
+  ModalContent,
+} from "@nextui-org/react";
 import Link from "next/link";
-import { Button } from "@nextui-org/react";
 import EditVenue from "./EditVenue";
 export default function ListVenue({ venue, setVenueHasBeenUpdate }) {
   const [showEditMenu, setShowEditMenu] = useState(false);
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const [openedVenue, setOpenedVenue] = useState();
+
   const handleEditClick = () => {
     setShowEditMenu(true);
   };
@@ -14,11 +29,20 @@ export default function ListVenue({ venue, setVenueHasBeenUpdate }) {
   };
 
   return (
-    <Card className="py-4">
-      <Link href={`/venues/${venue.venue_id}`}>
+    <>
+      <Card
+        className="py-4"
+        isPressable={true}
+        onPress={() => {
+          setOpenedVenue(venue);
+          onOpen();
+        }}
+      >
         <CardHeader className="h-20 flex-col items-start overflow-hidden px-4 pb-0 pt-2">
           <p className="text-tiny font-bold uppercase">{venue.name}</p>
-          <small className="text-default-500">{venue.description}</small>
+          <small className="line-clamp-1 text-default-500">
+            {venue.description}
+          </small>
           {venue.isUserVenue && (
             <p className="text-tiny font-bold uppercase">Your Venue</p>
           )}
@@ -30,32 +54,71 @@ export default function ListVenue({ venue, setVenueHasBeenUpdate }) {
             src={venue.photos}
           />
         </CardBody>
-      </Link>
-      <CardBody className="h-100 overflow-visible pb-2 pt-5">
-        <p className="text-tiny font-bold uppercase">{venue.address_1}</p>
-        <p className="text-tiny font-bold uppercase">{venue.postcode}</p>
-        <p className="text-tiny font-bold uppercase">{venue.city}</p>
-        <div className="mt-3">
-          {venue.isUserVenue && (
-            <Button
-              onClick={handleEditClick}
-              className="w-full"
-              color="primary"
-              radius="lg"
-            >
-              Edit
-            </Button>
+
+        <CardBody className="h-100 overflow-visible pb-2 pt-5">
+          <p className="text-tiny font-bold uppercase">{venue.address_1}</p>
+          <p className="text-tiny font-bold uppercase">{venue.postcode}</p>
+          <p className="text-tiny font-bold uppercase">{venue.city}</p>
+          <div className="mt-3">
+            {venue.isUserVenue && (
+              <Button
+                onClick={handleEditClick}
+                className="w-full"
+                color="primary"
+                radius="lg"
+              >
+                Edit
+              </Button>
+            )}
+          </div>
+          {showEditMenu === true ? (
+            <EditVenue
+              setVenueHasBeenUpdate={setVenueHasBeenUpdate}
+              handleEditClose={handleEditClose}
+              userId={venue.founder_id}
+              venue_id={venue.venue_id}
+            />
+          ) : null}
+        </CardBody>
+      </Card>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        onOpenChange={onOpenChange}
+        size="4xl"
+        backdrop="blur"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>{openedVenue.name}</ModalHeader>
+              <ModalBody>
+                <img src={openedVenue.photos} alt="" />
+                <p>{openedVenue.description}</p>
+              </ModalBody>
+              <ModalFooter>
+                <p>{openedVenue.price ? `£${openedVenue.price}` : "FREE"}</p>
+                <Link href="/host/event">
+                  <Button
+                    onPress={() => {
+                      const eventForm = JSON.parse(
+                        localStorage.getItem("eventFormDetails"),
+                      );
+                      eventForm.venue_id = venue.venue_id;
+                      localStorage.setItem(
+                        "eventFormDetails",
+                        JSON.stringify(eventForm),
+                      );
+                    }}
+                  >
+                    Choose venue
+                  </Button>
+                </Link>
+              </ModalFooter>
+            </>
           )}
-        </div>
-        {showEditMenu === true ? (
-          <EditVenue
-            setVenueHasBeenUpdate={setVenueHasBeenUpdate}
-            handleEditClose={handleEditClose}
-            userId={venue.founder_id}
-            venue_id={venue.venue_id}
-          />
-        ) : null}
-      </CardBody>
-    </Card>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
