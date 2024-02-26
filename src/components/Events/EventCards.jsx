c659a2c4f3c56d358c9c2f41671d41b05163def5
JoeA <joealexanderadams@gmail.com>
2024-02-26 09:03:07 +0000

    add css to Events

---
+++ src/components/Events/EventCards.jsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { supabaseAuth } from "@/utils/supabaseClient";
+import {
+  Card,
+  CardBody,
+  Image,
+  CardFooter,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+  useDisclosure,
+  ModalContent,
+} from "@nextui-org/react";
+import moment from "moment";
+import { useEffect, useState } from "react";
+
+export default function EventCards({ item, showDelete }) {
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const [openedEvent, setOpenedEvent] = useState();
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  useEffect(() => {}, [isDeleted]);
+
+  const handleDelete = () => {
+    console.log(item.event_id);
+    supabaseAuth.from("events").delete().eq("event_id", item.event_id).then();
+    setIsDeleted(true)
+  };
+
+  return isDeleted ? null : (
+    <>
+        <div className="flex-grow-1 ">
+          <Card
+            className="w-96"
+            isPressable={true}
+            onPress={() => {
+              setOpenedEvent(item);
+              onOpen();
+            }}
+          >
+            <CardBody>
+              <Image
+                className="w-dvw h-60 object-cover"
+                src={item.photos}
+                alt=""
+              />
+              <CardFooter>
+                <div className="flex-grow px-2">
+                  <h2 className="font-bold">{item.name}</h2>
+                  <p className="line-clamp-1">{item.description}</p>
+                  <div className="flex font-medium">
+                    <p className="flex-grow">
+                      Starts:{" "}
+                      {`${moment(item.start_date).format("DD/MM/YYYY")}, ${moment(item.start_date).format("HH:mm")}`}
+                    </p>
+                    <p>{item.event_price ? `£${item.event_price}` : "FREE"}</p>
+                  </div>
+                </div>
+              </CardFooter>
+            </CardBody>
+          </Card>
+          <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpenChange={onOpenChange}
+            size="4xl"
+            backdrop="blur"
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader>{openedEvent.name}</ModalHeader>
+                  <ModalBody>
+                    <img src={openedEvent.photos} alt="" />
+                    <p>{openedEvent.description}</p>
+                  </ModalBody>
+                  <ModalFooter>
+                    <p className="flex-grow">
+                      Starts:{" "}
+                      {`${moment(openedEvent.start_date).format("DD/MM/YYYY")}, ${moment(openedEvent.start_date).format("HH:mm")}`}
+                    </p>
+                    <p>
+                      {openedEvent.event_price
+                        ? `£${openedEvent.event_price}`
+                        : "FREE"}
+                    </p>
+                    {showDelete ? (
+                      <Button onPress={handleDelete}>Delete</Button>
+                    ) : (
+                      <Button>Book now</Button>
+                    )}
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+        </div>
+      </>)
+}
