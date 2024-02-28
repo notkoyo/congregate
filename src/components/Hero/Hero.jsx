970bd6d20e004c6e4f087571b4bd8df1b172023a
notkoyo <135956230+notkoyo@users.noreply.github.com>
2024-02-28 14:34:04 +0000

    bug fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -39,7 +39,6 @@ export default function Hero() {
         const currentEvents = data.filter(
           (e) => new Date(e.start_date) >= Date.now(),
         );
-        console.log(currentEvents);
         setEvents(currentEvents);
       });
   }, []);
@@ -110,7 +109,7 @@ export default function Hero() {
           Current Events
         </h2>
         {events.length > 0 ? (
-          <div className="lg:px-30 grid grid-cols-1 gap-4 px-20 py-10 pb-24 font-satoshi sm:px-40 md:grid-cols-3 md:px-2 lg:grid-cols-3 xl:px-[8rem]">
+          <div className="font-satoshi flex justify-center items-center gap-[2rem] flex-col xl:flex-row">
             {[events[0], events[1], events[2]].map((event) => {
               console.log(event);
               return (
@@ -160,216 +159,13 @@ export default function Hero() {
           </div>
         )}
         {events.length > 0 ? (
-          <div className="flex items-center justify-center pb-20">
+          <div className="flex items-center justify-center py-20">
             <Button as={Link} href="/meet" size="lg" className="font-semibold">
               View these events and more!
             </Button>
           </div>
         ) : undefined}
       </div>
-      <footer>
-        <div className="flex min-h-36 flex-col items-center justify-center px-20 md:flex-row md:justify-between">
-          <span className="hidden font-satoshi font-semibold text-gray-400/40 md:block">
-            © 2024 Congregate Ltd. All rights reserved.
-          </span>
-          <div>
-            <ul className="flex gap-x-10">
-              <li>
-                <Popover>
-                  <PopoverTrigger>
-                    <Link
-                      as="button"
-                      className="text-black transition duration-300 hover:text-black/40"
-                    >
-                      <DiamondIcon />
-                    </Link>
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <div className="p-5 font-satoshi font-semibold">
-                      <h3 className="cursor-default text-xl text-black">
-                        Check out the developers!
-                      </h3>
-                      <Divider className="my-4" />
-                      <ul className="flex flex-col gap-2">
-                        <li>
-                          <Popover showArrow placement="bottom">
-                            <PopoverTrigger>
-                              <User
-                                isFocusable
-                                as="button"
-                                name="Kaiden Riley"
-                                description="Junior Fullstack Developer"
-                                className="transition-transform"
-                                avatarProps={{
-                                  src: "https://media.licdn.com/dms/image/D4E03AQFGntLsoNhW4Q/profile-displayphoto-shrink_200_200/0/1708263153007?e=1714003200&v=beta&t=M6PS1qWGGQ5pmEr1NPfm68hco8DUwqGC4XpX1wj_-cA",
-                                }}
-                              />
-                            </PopoverTrigger>
-                            <PopoverContent className="p-1">
-                              <UserCard
-                                name={"Kaiden Riley"}
-                                emoji={"🚀"}
-                                linkedInHref="https://linkedin.com/in/kaiden-riley"
-                                gitHubHref="https://github.com/notkoyo"
-                                bio={
-                                  "Junior Software Developer | React & TypeScript Enjoyer | Northcoders Graduate | Travelling"
-                                }
-                                imageSrc={
-                                  "https://media.licdn.com/dms/image/D4E03AQFGntLsoNhW4Q/profile-displayphoto-shrink_200_200/0/1708263153007?e=1714003200&v=beta&t=M6PS1qWGGQ5pmEr1NPfm68hco8DUwqGC4XpX1wj_-cA"
-                                }
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </li>
-                        <li>
-                          <Popover showArrow placement="bottom">
-                            <PopoverTrigger>
-                              <User
-                                as="button"
-                                name="Joe Man"
-                                description="Junior Fullstack Developer"
-                                className="transition-transform"
-                                avatarProps={{
-                                  src: "https://media.licdn.com/dms/image/D4E35AQHTq_8idxygmQ/profile-framedphoto-shrink_200_200/0/1705660145251?e=1709222400&v=beta&t=359Yh0ZgQfaK0zxAFrWHxCU-XTEWIHyIsxqQsk0PUlw",
-                                }}
-                              />
-                            </PopoverTrigger>
-                            <PopoverContent className="p-1">
-                              <UserCard
-                                name={"Joe Man"}
-                                emoji={"🧠"}
-                                linkedInHref="https://linkedin.com/in/joe-man-60b792194/"
-                                gitHubHref="https://github.com/joe-man"
-                                bio={
-                                  "Junior Software Developer | RPA Developer | BSc Mathematics"
-                                }
-                                imageSrc={
-                                  "https://media.licdn.com/dms/image/D4E35AQHTq_8idxygmQ/profile-framedphoto-shrink_200_200/0/1705660145251?e=1709222400&v=beta&t=359Yh0ZgQfaK0zxAFrWHxCU-XTEWIHyIsxqQsk0PUlw"
-                                }
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </li>
-                        <li>
-                          <Popover showArrow placement="bottom">
-                            <PopoverTrigger>
-                              <User
-                                as="button"
-                                name="Anthony Moran"
-                                description="Junior Fullstack Developer"
-                                className="transition-transform"
-                                avatarProps={{
-                                  src: "https://avatars.githubusercontent.com/u/117123909?v=4",
-                                }}
-                              />
-                            </PopoverTrigger>
-                            <PopoverContent className="p-1">
-                              <UserCard
-                                name={"Anthony Moran"}
-                                emoji={"🐛"}
-                                linkedInHref="https://linkedin.com/in/anthonymmoran/"
-                                gitHubHref="https://github.com/tonymm55"
-                                bio={
-                                  "Junior Software Developer, Northcoders software development bootcamp graduate. BEng(Hons) Mechanical Engineering, MBA Chemical Engineering."
-                                }
-                                imageSrc={
-                                  "https://avatars.githubusercontent.com/u/117123909?v=4"
-                                }
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </li>
-                        <li>
-                          <Popover showArrow placement="bottom">
-                            <PopoverTrigger>
-                              <User
-                                as="button"
-                                name="Joe Adams"
-                                description="Junior Fullstack Developer"
-                                className="transition-transform"
-                                avatarProps={{
-                                  src: "",
-                                }}
-                              />
-                            </PopoverTrigger>
-                            <PopoverContent className="p-1">
-                              <UserCard
-                                name={"Joe Adams"}
-                                emoji={"🌍"}
-                                linkedInHref="https://linkedin.com/in/joe-adams-7592621b5/"
-                                gitHubHref="https://github.com/JoeAAdams"
-                                bio={defaultBio}
-                                imageSrc={""}
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </li>
-                        <li>
-                          <Popover showArrow placement="bottom">
-                            <PopoverTrigger>
-                              <User
-                                as="button"
-                                name="Dmytro Pen"
-                                description="Junior Fullstack Developer"
-                                className="transition-transform"
-                                avatarProps={{
-                                  src: "https://avatars.githubusercontent.com/u/102535430?v=4",
-                                }}
-                              />
-                            </PopoverTrigger>
-                            <PopoverContent className="p-1">
-                              <UserCard
-                                name={"Dmytro Pen"}
-                                emoji={"🎧"}
-                                linkedInHref="https://www.linkedin.com/in/dmytro-pen-a79988257/"
-                                gitHubHref="https://github.com/PENbDM"
-                                bio={defaultBio}
-                                imageSrc={
-                                  "https://avatars.githubusercontent.com/u/102535430?v=4"
-                                }
-                              />
-                            </PopoverContent>
-                          </Popover>
-                        </li>
-                      </ul>
-                    </div>
-                  </PopoverContent>
-                </Popover>
-              </li>
-              <li>
-                <Link
-                  className="text-black transition duration-300 hover:text-black/40"
-                  showAnchorIcon
-                  anchorIcon={<GitHubIcon />}
-                  href="https://github.com/notkoyo/congregate-app"
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <Link
-                  className="text-black transition duration-300 hover:text-black/40"
-                  showAnchorIcon
-                  anchorIcon={<FacebookIcon />}
-                  href="https://facebook.com"
-                  target="_blank"
-                />
-              </li>
-              <li>
-                <Link
-                  className="text-black transition duration-300 hover:text-black/40"
-                  showAnchorIcon
-                  anchorIcon={<TwitterIcon />}
-                  href="https://x.com"
-                  target="_blank"
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <span className="flex items-center justify-center pb-10 font-satoshi font-semibold text-gray-400/40 md:hidden">
-          © 2024 Congregate Ltd. All rights reserved.
-        </span>
-      </footer>
     </>
   );
 }
